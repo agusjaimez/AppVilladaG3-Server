0c5be8c3654df4bf4e61f326a4f0c3778dce18a4
FranMartiarena <40604256+FranMartiarena@users.noreply.github.com>
2020-08-10 18:28:18 -0300

    Añadi unos models

--- villadaApp/app/models.py
+++ villadaApp/app/models.py
@@ -48,13 +48,6 @@ class Preceptor(models.Model):
     last_name = models.CharField(max_length=30)
     dni = models.CharField(max_length=30)
 
-class Comunicado(models.Model):
-    fecha = models.dateField()
-    remitente = models.CharField(max_length=30)
-    directivo = models.ForeignKey(Directivo, on_delete=models.CASCADE)
-    mensaje = models.TextField()
-    cursos = models.ManyToManyField(Curso)
-
 class Alumno(models.Model):
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
@@ -76,7 +69,34 @@ class Formulario(models.Model):
         F1 = 'F1', _('Formulario 1')
         F2 = 'F2', _('Formulario 2')
         F3 = 'F3', _('Formulario 3')
-        
+
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
     descripcion = models.TextField()
     tipo_form = models.CharField(max_length=2, choices=TipoForm.choices, default=TipoForm.F1)
+
+class SolicitudReunion(models.Model):
+    #Se debe agregar un estado de reunion(Aceptada, denegada, Procesando)
+    padre = models.ForeignKey(PadreTutor, on_delete=models.CASCADE)
+    fecha = models.dateField()
+    motivo = models.TextField()
+
+class ComunicadoCurso(models.Model):
+    fecha = models.dateField()
+    directivo = models.ForeignKey(Directivo, on_delete=models.CASCADE)
+    mensaje = models.TextField()
+    cursos = models.ManyToManyField(Curso)
+
+class ComunicadoCiclo(models.Model):
+    CICLOS = (
+    ("B", "Basico"),
+    ("A", "Avanzado"),
+)
+    fecha = models.dateField()
+    directivo = models.ForeignKey(Directivo, on_delete=models.CASCADE)
+    mensaje = models.TextField()
+    ciclo = models.CharField(max_length = 20 ,choices = CICLOS, default = 'B')
+
+class ComunicadoGeneral(models.Model):
+    fecha = models.dateField()
+    directivo = models.ForeignKey(Directivo, on_delete=models.CASCADE)
+    mensaje = models.TextField()
