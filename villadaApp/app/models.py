54a352caf1be8e6a2fd19a5f9c35cacb05ac1417
NahuelFredes <nahuelfredes2018@gmail.com>
2020-08-11 11:40:41 -0300

    arreglado problemas de formato en models

--- villadaApp/app/models.py
+++ villadaApp/app/models.py
@@ -5,33 +5,33 @@ import datetime
 class Curso(models.Model):
 
     class Cursos(models.TextChoices):
-        primeroA = '1A', _('Primero A')
-        primeroB = '1B', _('Primero B')
-        primeroC = '1C', _('Primero C')
+        primeroA = '1A', ('Primero A')
+        primeroB = '1B', ('Primero B')
+        primeroC = '1C', ('Primero C')
 
-        segundoA = '2A', _('Segundo A')
-        segundoB = '2B', _('Segundo B')
-        segundoC = '2C', _('Segundo C')
+        segundoA = '2A', ('Segundo A')
+        segundoB = '2B', ('Segundo B')
+        segundoC = '2C', ('Segundo C')
 
-        terceroA = '3A', _('Tercero A')
-        terceroB = '3B', _('Tercero B')
-        terceroC = '3C', _('Tercero C')
+        terceroA = '3A', ('Tercero A')
+        terceroB = '3B', ('Tercero B')
+        terceroC = '3C', ('Tercero C')
 
-        cuartoA = '4A', _('Cuarto A')
-        cuartoB = '4B', _('Cuarto B')
-        cuartoC = '4C', _('Cuarto C')
+        cuartoA = '4A', ('Cuarto A')
+        cuartoB = '4B', ('Cuarto B')
+        cuartoC = '4C', ('Cuarto C')
 
-        quintoA = '5A', _('Quinto A')
-        quintoB = '5B', _('Quinto B')
-        quintoC = '5C', _('Quinto C')
+        quintoA = '5A', ('Quinto A')
+        quintoB = '5B', ('Quinto B')
+        quintoC = '5C', ('Quinto C')
 
-        sextoA = '6A', _('Sexto A')
-        sextoB = '6B', _('Sexto B')
-        sextoC = '6C', _('Sexto C')
+        sextoA = '6A', ('Sexto A')
+        sextoB = '6B', ('Sexto B')
+        sextoC = '6C', ('Sexto C')
 
-        septimoA = '7A', _('Septimo A')
-        septimoB = '7B', _('Septimo B')
-        septimoC = '7C', _('Septimo C')
+        septimoA = '7A', ('Septimo A')
+        septimoB = '7B', ('Septimo B')
+        septimoC = '7C', ('Septimo C')
 
     curso = models.CharField(max_length=2, choices=Cursos.choices, default=Cursos.primeroA)
 
@@ -43,7 +43,7 @@ class Directivo(models.Model):
     password = models.CharField(max_length=30)
 
 class Preceptor(models.Model):
-    curso = models.ManyToManyField(Curso, on_delete=models.SET_NULL)
+    curso = models.ManyToManyField(Curso)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     dni = models.CharField(max_length=30)
@@ -66,9 +66,9 @@ class PadreTutor(models.Model):
 class Formulario(models.Model):
 
     class TipoForm(models.TextChoices):
-        F1 = 'F1', _('Formulario 1')
-        F2 = 'F2', _('Formulario 2')
-        F3 = 'F3', _('Formulario 3')
+        F1 = 'F1', ('Formulario 1')
+        F2 = 'F2', ('Formulario 2')
+        F3 = 'F3', ('Formulario 3')
 
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
     descripcion = models.TextField()
@@ -77,11 +77,11 @@ class Formulario(models.Model):
 class SolicitudReunion(models.Model):
     #Se debe agregar un estado de reunion(Aceptada, denegada, Procesando)
     padre = models.ForeignKey(PadreTutor, on_delete=models.CASCADE)
-    fecha = models.dateField()
+    fecha = models.DateField()
     motivo = models.TextField()
 
 class ComunicadoCurso(models.Model):
-    fecha = models.dateField()
+    fecha = models.DateField()
     directivo = models.ForeignKey(Directivo, on_delete=models.CASCADE)
     mensaje = models.TextField()
     cursos = models.ManyToManyField(Curso)
@@ -91,12 +91,12 @@ class ComunicadoCiclo(models.Model):
     ("B", "Basico"),
     ("A", "Avanzado"),
 )
-    fecha = models.dateField()
+    fecha = models.DateField()
     directivo = models.ForeignKey(Directivo, on_delete=models.CASCADE)
     mensaje = models.TextField()
     ciclo = models.CharField(max_length = 20 ,choices = CICLOS, default = 'B')
 
 class ComunicadoGeneral(models.Model):
-    fecha = models.dateField()
+    fecha = models.DateField()
     directivo = models.ForeignKey(Directivo, on_delete=models.CASCADE)
     mensaje = models.TextField()
