0759ed1dc1c38a940feb0fa94c7a680afea87f27
Tomás Tissera <tomastissefa@gmail.com>
2020-08-12 17:48:01 -0300

    login-navbar
    Se ah realizado el log in donde se logiaran los usuarios , y ademas un navbar para navegar por los distintos templates que se crearon

--- villadaApp/app/views.py
+++ villadaApp/app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-
-# Create your views here.
-def home_view(request):
-    # render function takes argument  - request
-    # and return HTML as response 
-    return render(request, "home.html")
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+def index(request):
+    return render(request,'index.html')
+@login_required
+def special(request):
+    return HttpResponse("You are logged in !")
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+def user_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if email=="grupo3@gmail.com"and password=="grupo3":
+            return render(request, 'comunicado.html', {})
+        else:
+            return HttpResponse("CASI bro")
+    else:
+        return render(request, 'login.html', {})
