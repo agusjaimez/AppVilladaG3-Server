883735c6665b1b652657901ab1c36cdb8be10c42
FranMartiarena <40604256+FranMartiarena@users.noreply.github.com>
2020-09-17 19:22:59 -0300

    Ahora se pueden eliminar los comunicados

--- villadaApp/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from app import views
-# SET THE NAMESPACE!
-app_name = 'app'
-# Be careful setting the name to just /login use userlogin instead!
-urlpatterns=[
-    
-]
