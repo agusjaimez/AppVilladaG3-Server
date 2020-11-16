aefac1b7a0e5b7a1688f7dbd2ad3bab10ccc854f
agusjaimez <agustinjaim@gmail.com>
2020-11-16 19:14:50 -0300

    Added hour in f2 description format

--- villadaApp/app/models.py
+++ villadaApp/app/models.py
@@ -170,9 +170,9 @@ class Formulario(models.Model):
         if self.tipo_form=='F1':
             txt= 'Córdoba, '+str(self.fecha)+'.\n Prof. '+str(Directivo.objects.get(cargo='DA'))+' \nMe dirijo a Ud. a los efectos de solicitarle la justificación de la/s inasistencia/s del Alumno '+str(self.alumno)+' del curso: '+str(self.alumno.curso)+' debido a: '+str(self.descripcion)+' los dias: '+str(self.dias)
         elif self.tipo_form=='F2':
-            txt= 'Córdoba, '+str(self.fecha)+'.\n Prof. '+str(Directivo.objects.get(cargo='DA'))+' \nMe dirijo a Ud. a los efectos de autorizar, en el día de la fecha , al Alumno '+str(self.alumno)+' del curso: '+str(self.alumno.curso)+'a retirarse del Establecimiento por sus propios medios, por ausencia del docente, u otra causa imprevista que pudiere surgir. Sin otro particular le saludo atte. '
+            txt= 'Córdoba, '+str(self.fecha)+'.\n Prof. '+str(Directivo.objects.get(cargo='DA'))+' \nMe dirijo a Ud. a los efectos de autorizar, en el día de la fecha , al Alumno '+str(self.alumno)+' del curso: '+str(self.alumno.curso)+'a retirarse del Establecimiento por sus propios medios, por ausencia del docente, u otra causa imprevista que pudiere surgir, a las '+ str(self.hora)+'hs. Sin otro particular le saludo atte. '
         else:
-            txt='Córdoba, '+str(self.fecha)+'.\n Prof. '+str(Directivo.objects.get(cargo='DA'))+' \nMe dirijo a Ud. a los efectos de autorizar, en el día de la fecha , al Alumno '+str(self.alumno)+' del curso: '+str(self.alumno.curso)+'a retirarse del Establecimiento por sus propios medios,'+str(self.alumno)+' del curso: '+str(self.alumno.curso)+' a las '+ str(self.hora)+'hs debido a: '+str(self.descripcion)
+            txt='Córdoba, '+str(self.fecha)+'.\n Prof. '+str(Directivo.objects.get(cargo='DA'))+' \nMe dirijo a Ud. a los efectos de autorizar, en el día de la fecha , al Alumno '+str(self.alumno)+' del curso: '+str(self.alumno.curso)+'a retirarse del Establecimiento por sus propios medios, a las '+ str(self.hora)+'hs debido a: '+str(self.descripcion)
         self.descripcion = txt
         super().save(*args, **kwargs)
 
